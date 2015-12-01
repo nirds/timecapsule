c8116aae04e3505a62f3ef27634d2ebc3e701f46
Risa B <rbatta@gmail.com>
2015-12-01 11:52:41 -0800

    slight refactoring to switch to string format

--- lib/timecapsule/tasks.rb
+++ lib/timecapsule/tasks.rb
@@ -1,17 +1,17 @@
 namespace :timecapsule do
-  model_list = Dir['app/models/*.rb'].map{ |file| File.basename(file, '.*').to_sym }
+  model_list = Dir['app/models/*.rb'].map{ |file| File.basename(file, '.*') }
 
   model_list.each do |klass|
-    desc "#{klass} model name"
-    task klass => :environment do
+    desc "Generates CSV dump of all #{klass} records"
+    task klass.pluralize.to_sym => :environment do
       run_timecapsule_for(klass)
     end
   end
 
   def run_timecapsule_for(klass)
     Rails.application.eager_load!
-    if ActiveRecord::Base.descendants.map(&:name).include? klass.to_s.classify
-      Timecapsule.export_model(klass.to_s.classify.constantize)
+    if ActiveRecord::Base.descendants.map(&:name).include? klass.classify
+      Timecapsule.export_model(klass.classify.constantize)
     end
   end
 end
