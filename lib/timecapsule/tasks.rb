c441fa9248fe5adb0db1088ec4c7ebc75369e15d
Risa B <rbatta@gmail.com>
2015-12-01 11:44:41 -0800

    create working rake task

--- lib/timecapsule/tasks.rb
+++ lib/timecapsule/tasks.rb
@@ -1,14 +1,17 @@
 namespace :timecapsule do
-  # Rails.application.eager_load! (breaks w/ admin/admin_controller and we're only running rake and not rails)
-  # model_list = ActiveRecord::Base.connection.tables.map(&:to_sym)
-  # ActiveRecord::Base.descendants.map(&:name).map(&:to_sym)
-  # above didn't work because activerecord didn't load them up (they weren't being seen)
-  # its not loading up the rails portion
-  # dir works only if the models are xyz.rb and happily ignores other files
   model_list = Dir['app/models/*.rb'].map{ |file| File.basename(file, '.*').to_sym }
+
   model_list.each do |klass|
-    desc "#{klass} model name - #{model_list.inspect}" # inspect the array
-    task klass do
+    desc "#{klass} model name"
+    task klass => :environment do
+      run_timecapsule_for(klass)
+    end
+  end
+
+  def run_timecapsule_for(klass)
+    Rails.application.eager_load!
+    if ActiveRecord::Base.descendants.map(&:name).include? klass.to_s.classify
+      Timecapsule.export_model(klass.to_s.classify.constantize)
     end
   end
 end
